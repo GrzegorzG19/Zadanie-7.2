1fcaaadaef2697a5c8c0d035f853a1d57a756679
Grzegorz <grzegorzgolan18@gmail.com>
2019-05-23 16:39:57 +0200

    2

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        double a= 0;
+        while(a <= 3.1){
+            System.out.printf("%.1f ",a);
+            a+=0.1;
+        }
+        a= 0;
+        System.out.println();
+       do {
+            System.out.printf("%.1f ",a);
+            a+=0.1;
+        } while(a <= 3.1);
+
+    }
+}
